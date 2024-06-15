4cf609c02e4a79d986dc4a2f6ce462dbbcb694f5
IIlya-ipti <zuc.120900@gmail.com>
2024-06-15 23:20:48 +0300

    fix

--- src/main/Game/Constants.hpp
+++ src/main/Game/Constants.hpp
@@ -49,7 +49,7 @@ constexpr double MOB_MAX_ATTACK_HEALTH_TO_AGGRESSIVE = 20;
 constexpr double MOB_WEAPON_POS_X = 0;
 constexpr double MOB_WEAPON_POS_Y = -1000;
 
-const std::string BUG = "/home/mnzconst/Desktop/mse/sem_2/sd/Rougelike-Game-/src/main/Assets/";
+const std::string BUG = "src/main/Assets/";
 //  /home/mnzconst/Desktop/mse/sem_2/sd/Rougelike-Game-/src/main/Assets/_Attack.png
 
 // MENU_SETTINGS
